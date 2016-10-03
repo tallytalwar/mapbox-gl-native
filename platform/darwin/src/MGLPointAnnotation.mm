2a93ddcff9d2d04ed2db10b77f8b95ed95aa9490
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2016-10-03 11:33:38 +0200

    [ios, macos] MGLShape and MGLPointAnnotation now conforms to NSSecureCoding

--- platform/darwin/src/MGLPointAnnotation.mm
+++ platform/darwin/src/MGLPointAnnotation.mm
@@ -9,6 +9,51 @@
 
 @synthesize coordinate;
 
++ (BOOL)supportsSecureCoding
+{
+    return YES;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    if (self = [super initWithCoder:coder]) {
+        self.coordinate = CLLocationCoordinate2DMake([coder decodeDoubleForKey:@"coordinateLatitude"],
+                                                     [coder decodeDoubleForKey:@"coordinateLongitude"]);
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    [super encodeWithCoder:coder];
+    [coder encodeDouble:coordinate.latitude forKey:@"coordinateLatitude"];
+    [coder encodeDouble:coordinate.longitude forKey:@"coordinateLongitude"];
+}
+
+- (BOOL)isEqual:(id)other
+{
+    if (other == self)
+    {
+        return YES;
+    }
+    if ( ! [other isKindOfClass:[self class]])
+    {
+        return NO;
+    }
+    
+    MGLPointAnnotation *otherAnnotation = other;
+    return (self.coordinate.latitude == otherAnnotation.coordinate.latitude
+            && self.coordinate.longitude == otherAnnotation.coordinate.longitude
+            && [self.title isEqualToString:otherAnnotation.title]
+            && [self.subtitle isEqualToString:otherAnnotation.subtitle]);
+}
+
+- (NSUInteger)hash
+{
+    return @(self.coordinate.latitude).hash+@(self.coordinate.longitude).hash
+    +self.title.hash+self.subtitle.hash;
+}
+
 - (NSString *)description
 {
     return [NSString stringWithFormat:@"<%@: %p; title = %@; subtitle = %@; coordinate = %f, %f>",
